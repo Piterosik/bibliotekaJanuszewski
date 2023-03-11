549d296c906aeb49c98aba33bb2a627a38599eb9
Piotr Januszewski <piotr.januszewski@codeagency.pl>
2023-03-11 02:45:11 +0100

    Added functionality for W option in menu

--- bibAdmJanuszewski/Program.cs
+++ bibAdmJanuszewski/Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using bibModelJanuszewski;
-
-
-
+using bibModelJanuszewski.Model;
+using ConsoleTables;
 
 namespace bibAdmJanuszewski
 {
     class Program
     {
+        enum SHOWMETHOD
+        {
+            XDOC,
+            XS
+        }
         static void Main(string[] args)
         {
             //var autor1 = new bibModelJanuszewski.Model.AutorzyAutor()
@@ -23,7 +27,7 @@ namespace bibAdmJanuszewski
 
             Console.WriteLine("Hello, World!");
 
-            BDLibrary db = new BDLibrary(
+            BDLibrary db = new(
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\__ukw"
                 );
 
@@ -42,6 +46,7 @@ namespace bibAdmJanuszewski
                     case 'w':
                     case 'W':
                         Console.WriteLine("Wybrałeś opcję W");
+                        ShowData(db, SHOWMETHOD.XS);
                         break;
                     case 'a':
                     case 'A':
@@ -76,6 +81,61 @@ namespace bibAdmJanuszewski
                 Console.WriteLine(option);
             }
             Console.Write("Wybierz opcje: ");
+        }
+
+        static void ShowData(BDLibrary db, SHOWMETHOD method = SHOWMETHOD.XDOC)
+        {
+            if (method == SHOWMETHOD.XDOC)
+            {
+                foreach (string model in db.ReportData())
+                {
+                    Console.WriteLine(model);
+                    Console.WriteLine();
+                }
+            }
+            else if (method == SHOWMETHOD.XS)
+            {
+                Console.WriteLine("Autorzy:");
+                Autorzy autorzy = db.ReportData2<Autorzy>();
+                if (autorzy != null && autorzy.Autor.Length > 0)
+                {
+                    var table = new ConsoleTable("ID", "Nazwisko", "Imie", "RokUrodzenia");
+                    foreach (AutorzyAutor autor in autorzy.Autor)
+                    {
+                        table.AddRow(autor.id, autor.nazwisko, autor.imię, autor.rokUr);
+                    }
+                    table.Write(Format.Alternative);
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Wydawcy:");
+                Wydawcy wydawcy = db.ReportData2<Wydawcy>();
+                if (autorzy != null && autorzy.Autor.Length > 0)
+                {
+                    var table = new ConsoleTable("ID", "Nazwa", "Strona");
+                    foreach (WydawcyWydawca wydawca in wydawcy.Wydawca)
+                    {
+                        table.AddRow(wydawca.id, wydawca.nazwa, wydawca.strona);
+                    }
+                    table.Write(Format.Alternative);
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Ksiazki:");
+                Ksiazki ksiazki = db.ReportData2<Ksiazki>();
+                if (autorzy != null && autorzy.Autor.Length > 0)
+                {
+                    var table = new ConsoleTable("ID", "Tytul", "ISBN", "Cena", "idAutora", "idWydawcy");
+                    foreach (KsiazkiKsiazka ksiazka in ksiazki.Ksiazka)
+                    {
+                        table.AddRow(ksiazka.id, ksiazka.tytul, ksiazka.ISBN, ksiazka.cena, ksiazka.idAutora, ksiazka.idWydawnictwa);
+                    }
+                    table.Write(Format.Alternative);
+                    Console.WriteLine();
+                }
+
+            }
+
         }
     }
 }
--- bibModelJanuszewski/BDLibrary.cs
+++ bibModelJanuszewski/BDLibrary.cs
@@ -8,181 +8,208 @@ using System.Xml.Serialization;
 
 namespace bibModelJanuszewski
 {
-	public class BDLibrary
-	{
-		readonly string authorsFile, publishersFile, booksFile;
+    public class BDLibrary
+    {
+        readonly string authorsFile, publishersFile, booksFile;
 
-		public BDLibrary(string path, string authorsFile = null, string publishersFile = null, string booksFile = null)
-		{
-			if (authorsFile == null) authorsFile = DefaultFileNames.defAuthors;
-			if (publishersFile == null) publishersFile = DefaultFileNames.defPublishers;
-			if (booksFile == null) booksFile = DefaultFileNames.defBooks;
+        public BDLibrary(string path, string authorsFile = null, string publishersFile = null, string booksFile = null)
+        {
+            if (authorsFile == null) authorsFile = DefaultFileNames.defAuthors;
+            if (publishersFile == null) publishersFile = DefaultFileNames.defPublishers;
+            if (booksFile == null) booksFile = DefaultFileNames.defBooks;
 
-			this.authorsFile = path + "\\" + authorsFile + ".xml";
-			this.publishersFile = path + "\\" + publishersFile + ".xml";
-			this.booksFile = path + "\\" + booksFile + ".xml";
-		}
+            this.authorsFile = path + "\\" + authorsFile + ".xml";
+            this.publishersFile = path + "\\" + publishersFile + ".xml";
+            this.booksFile = path + "\\" + booksFile + ".xml";
+        }
 
-		public bool TestData()
-		{
-			Autorzy autorzy = new Autorzy
-			{
-				Autor = new AutorzyAutor[]
-			{
-				new AutorzyAutor() { id = 1, imię = "Jan", nazwisko = "Kowalski", rokUr = 1999 },
-				new AutorzyAutor() { id = 2, imię = "Adam", nazwisko = "Nowak", rokUr = 1999 },
-				new AutorzyAutor() { id = 3, imię = "Ewelina", nazwisko = "Kątna", rokUr = 1999 },
-				new AutorzyAutor() { id = 4, imię = "Dawid", nazwisko = "Kopacz", rokUr = 1999 },
-				new AutorzyAutor() { id = 5, imię = "Piotr", nazwisko = "Januszewski", rokUr = 1997 },
-			}
-			};
+        public bool TestData()
+        {
+            Autorzy autorzy = new Autorzy
+            {
+                Autor = new AutorzyAutor[]
+            {
+                new AutorzyAutor() { id = 1, imię = "Jan", nazwisko = "Kowalski", rokUr = 1999 },
+                new AutorzyAutor() { id = 2, imię = "Adam", nazwisko = "Nowak", rokUr = 1999 },
+                new AutorzyAutor() { id = 3, imię = "Ewelina", nazwisko = "Kątna", rokUr = 1999 },
+                new AutorzyAutor() { id = 4, imię = "Dawid", nazwisko = "Kopacz", rokUr = 1999 },
+                new AutorzyAutor() { id = 5, imię = "Piotr", nazwisko = "Januszewski", rokUr = 1997 },
+            }
+            };
 
-			Console.WriteLine("Saving authors file: " + authorsFile);
-			if (!SaveFile(authorsFile, autorzy))
-			{
-				return false;
-			}
+            Console.WriteLine("Saving authors file: " + authorsFile);
+            if (!SaveFile(authorsFile, autorzy))
+            {
+                return false;
+            }
 
-			Wydawcy wydawcy = new Wydawcy
-			{
-				Wydawca = new WydawcyWydawca[]
-			{
-				new WydawcyWydawca() { id=1, nazwa="Dom Wydawniczy Rebis", strona="www.rebis.com.pl" },
-				new WydawcyWydawca() { id=2, nazwa="Wydawnictwo Albatros", strona="www.wydawnictwoalbatros.com" },
-				new WydawcyWydawca() { id=3, nazwa="Wydawnictwo Czarne", strona="http://czarne.com.pl" },
-				new WydawcyWydawca() { id=4, nazwa="Wydawnictwo Literackie", strona="www.wydawnictwoliterackie.pl" },
-				new WydawcyWydawca() { id=5, nazwa="Fabryka Słów", strona="www.fabrykaslow.com.pl" }
-			}
-			};
+            Wydawcy wydawcy = new Wydawcy
+            {
+                Wydawca = new WydawcyWydawca[]
+            {
+                new WydawcyWydawca() { id=1, nazwa="Dom Wydawniczy Rebis", strona="www.rebis.com.pl" },
+                new WydawcyWydawca() { id=2, nazwa="Wydawnictwo Albatros", strona="www.wydawnictwoalbatros.com" },
+                new WydawcyWydawca() { id=3, nazwa="Wydawnictwo Czarne", strona="http://czarne.com.pl" },
+                new WydawcyWydawca() { id=4, nazwa="Wydawnictwo Literackie", strona="www.wydawnictwoliterackie.pl" },
+                new WydawcyWydawca() { id=5, nazwa="Fabryka Słów", strona="www.fabrykaslow.com.pl" }
+            }
+            };
 
-			Console.WriteLine("Saving publishers file: " + publishersFile);
-			if (!SaveFile(publishersFile, wydawcy))
-			{
-				return false;
-			}
+            Console.WriteLine("Saving publishers file: " + publishersFile);
+            if (!SaveFile(publishersFile, wydawcy))
+            {
+                return false;
+            }
 
-			Ksiazki ksiazki = new Ksiazki
-			{
-				Ksiazka = new KsiazkiKsiazka[]
-			{
-				new KsiazkiKsiazka() { id=1, tytul="Lalka", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=2, tytul="Wesele", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=3, tytul="Placówka", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=4, tytul="Inny świat", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=5, tytul="Powrót z gwiazd", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=6, tytul="Heban", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=7, tytul="Faraon", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=8, tytul="Ziemia obiecana", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=9, tytul="Ogniem i mieczem", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
-				new KsiazkiKsiazka() { id=10, tytul="Dzieła zebrane", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" }
-			}
-			};
+            Ksiazki ksiazki = new Ksiazki
+            {
+                Ksiazka = new KsiazkiKsiazka[]
+            {
+                new KsiazkiKsiazka() { id=1, tytul="Lalka", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=2, tytul="Wesele", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=3, tytul="Placówka", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=4, tytul="Inny świat", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=5, tytul="Powrót z gwiazd", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=6, tytul="Heban", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=7, tytul="Faraon", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=8, tytul="Ziemia obiecana", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=9, tytul="Ogniem i mieczem", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" },
+                new KsiazkiKsiazka() { id=10, tytul="Dzieła zebrane", cena=2.50f, idAutora="", idWydawnictwa="",ISBN="" }
+            }
+            };
 
-			Console.WriteLine("Saving books file: " + booksFile);
-			if (!SaveFile(booksFile, ksiazki))
-			{
-				return false;
-			}
+            Console.WriteLine("Saving books file: " + booksFile);
+            if (!SaveFile(booksFile, ksiazki))
+            {
+                return false;
+            }
 
-			return true;
+            return true;
 
-		}
+        }
 
-		private static bool SaveFile<T>(string path, T obj)
-		{
-			XmlSerializer xs = new XmlSerializer(typeof(T));
+        private static bool SaveFile<T>(string path, T obj)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
 
-			try
-			{
-				if (!File.Exists(path))
-				{
-					FileInfo fi = new FileInfo(path);
-					fi.Directory.Create();
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    FileInfo fi = new FileInfo(path);
+                    fi.Directory.Create();
 
-					using (StreamWriter s = new StreamWriter(path))
-					{
-						xs.Serialize(s, obj);
-					}
-				}
-				else
-				{
-					Console.WriteLine("File exists");
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.Error.WriteLine(ex.Message);
-				return false;
-			}
-			return true;
-		}
+                    using (StreamWriter s = new StreamWriter(path))
+                    {
+                        xs.Serialize(s, obj);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File exists");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
 
-		private static bool GenerateBooks(string path)
-		{
-			XDocument doc = new XDocument(
-				new XDeclaration("1.0", "utf-8", "no"),
-				new XComment("Przykładowe dane ksiązek"),
-				new XElement("Ksiazki",
-					new XAttribute("wersja", "2.0"),
-					new XElement("Ksiazka",
-						new XAttribute("id", "1"),
-						new XAttribute("tytul", "Lalka"),
-						new XAttribute("idAutora", ""),
-						new XAttribute("ISBN", ""),
-						new XAttribute("cena", ""),
-						new XAttribute("idWydawnictwa", "")
-					)
-				)
-			);
-
-
-			string[,] bookData = new string[,]
-			{
-				{"Bolesław Prus", "Lalka" },
-				{"Stanisław Wyspiański", "Wesele" },
-				{"Bolesław Prus", "Placówka" },
-				{"Gustaw Herling-Grudziński", "Inny świat" },
-				{"Stanisław Lem", "Powrót z gwiazd" },
-				{"Ryszard Kapuściński", "Heban" },
-				{"Bolesław Prus", "Faraon" },
-				{"Władysław Stanisław Reymont", "Ziemia obiecana" },
-				{"Henryk Sienkiewicz", "Ogniem i mieczem" },
-				{"Piotr Januszewski", "Dzieła zebrane" }
-			};
-
-			for (int i = 0; i < bookData.Length; i++)
-			{
-				doc.Root.Add(
-					new XElement("Ksiazka",
-						new XAttribute("id", i + 1),
-						new XAttribute("tytul", bookData[i, 1]),
-						new XAttribute("idAutora", ""),
-						new XAttribute("ISBN", ""),
-						new XAttribute("cena", 15.00f + (0.5f * i)),
-						new XAttribute("idWydawnictwa", "")
-					)
-				);
-			}
+        private static bool GenerateBooks(string path)
+        {
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "no"),
+                new XComment("Przykładowe dane ksiązek"),
+                new XElement("Ksiazki",
+                    new XAttribute("wersja", "2.0"),
+                    new XElement("Ksiazka",
+                        new XAttribute("id", "1"),
+                        new XAttribute("tytul", "Lalka"),
+                        new XAttribute("idAutora", ""),
+                        new XAttribute("ISBN", ""),
+                        new XAttribute("cena", ""),
+                        new XAttribute("idWydawnictwa", "")
+                    )
+                )
+            );
 
 
-			Console.WriteLine("Saving books file: " + path);
-			try
-			{
-				if (!File.Exists(path))
-				{
-					doc.Save(path);
-				}
-				else
-				{
-					Console.WriteLine("File exists");
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.Error.WriteLine(ex.Message);
-				return false;
-			}
-			return true;
-		}
-	}
+            string[,] bookData = new string[,]
+            {
+                {"Bolesław Prus", "Lalka" },
+                {"Stanisław Wyspiański", "Wesele" },
+                {"Bolesław Prus", "Placówka" },
+                {"Gustaw Herling-Grudziński", "Inny świat" },
+                {"Stanisław Lem", "Powrót z gwiazd" },
+                {"Ryszard Kapuściński", "Heban" },
+                {"Bolesław Prus", "Faraon" },
+                {"Władysław Stanisław Reymont", "Ziemia obiecana" },
+                {"Henryk Sienkiewicz", "Ogniem i mieczem" },
+                {"Piotr Januszewski", "Dzieła zebrane" }
+            };
+
+            for (int i = 0; i < bookData.Length; i++)
+            {
+                doc.Root.Add(
+                    new XElement("Ksiazka",
+                        new XAttribute("id", i + 1),
+                        new XAttribute("tytul", bookData[i, 1]),
+                        new XAttribute("idAutora", ""),
+                        new XAttribute("ISBN", ""),
+                        new XAttribute("cena", 15.00f + (0.5f * i)),
+                        new XAttribute("idWydawnictwa", "")
+                    )
+                );
+            }
+
+
+            Console.WriteLine("Saving books file: " + path);
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    doc.Save(path);
+                }
+                else
+                {
+                    Console.WriteLine("File exists");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public string[,] ReportData()
+        {
+            string[,] result = new string[,]
+            {
+                {"Autorzy:",XDocument.Load(authorsFile).ToString() },
+                {"Wydawcy:", XDocument.Load(publishersFile).ToString() },
+                {"Ksiazki:", XDocument.Load(booksFile).ToString() },
+            };
+            return result;
+        }
+
+        public T ReportData2<T>()
+        {
+            T result;
+
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+
+            string path = typeof(T) == typeof(Autorzy) ? authorsFile : typeof(T) == typeof(Wydawcy) ? publishersFile : booksFile;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                result = (T) xs.Deserialize(sr);
+            }
+
+            return result;
+        }
+    }
 }
